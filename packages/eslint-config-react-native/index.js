c2fe67d3c9debfaca377e62584a30fbe0b41bfb9
紫竹 <luozhu2021@gmail.com>
2025-02-17 01:09:29 +0800

    chore: 基于 @react-native/eslint-config 扩展

--- packages/eslint-config-react-native/index.js
+++ packages/eslint-config-react-native/index.js
@@ -1,24 +1,5 @@
 require('./patch');
 
 module.exports = {
-  extends: ['plugin:react-native/all', '@youngjuning/eslint-config-react-typescript'],
-  env: {
-    'react-native/react-native': true,
-  },
-  rules: {
-    'react-native/no-color-literals': 0,
-    'react-native/split-platform-components': 0,
-    'react-native/no-raw-text': 0,
-    'react-native/no-inline-styles': 1,
-  },
-  overrides: [
-    {
-      files: ['*.jsx', '*.js'],
-      parserOptions: {
-        babelOptions: {
-          presets: ['module:metro-react-native-babel-preset'],
-        },
-      },
-    },
-  ],
+  extends: ['@react-native', 'plugin:prettier/recommended', 'prettier'],
 };
